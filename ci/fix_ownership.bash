2c10701e3d3e12db3df5bb3ff29528841042cc74
James Orson <jamesaorson@gmail.com>
2022-09-24 22:35:09 -0700

    Add fix_ownership.bash

---
+++ ci/fix_ownership.bash
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+source cicada/ci/functions.bash
+
+log "Fixing ownership of mounted directories..."
+
+set -e
+
+if [ -z ${CICADA_JENKINS_USER} ];
+then
+	echo "Must define env var: CICADA_JENKINS_USER"
+	exit 1
+fi
+
+if [ -z ${CICADA_JENKINS_GROUP} ];
+then
+	echo "Must define env var: CICADA_JENKINS_GROUP"
+	exit 1
+fi
+
+chown -R ${CICADA_JENKINS_USER}:${CICADA_JENKINS_GROUP} .
+
+log "Successfully fixed ownership of mounted directories!"
